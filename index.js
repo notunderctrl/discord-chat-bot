c64d7a70bb494866e230ea3b0823240a70b9b000
notunderctrl <underctrl.io@gmail.com>
2023-03-01 21:23:05 +0300

    Update openai version

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ client.on('messageCreate', async (message) => {
     });
 
     const result = await openai.createCompletion({
-      model: 'gpt-3.5-turbo-0301',
+      model: 'gpt-3.5-turbo',
       prompt: `${conversationContext(client.user.tag)}
              ${conversationLog}
              ${client.user.tag}:
